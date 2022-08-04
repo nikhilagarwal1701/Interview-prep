1b23250995faf3f2b03a73200f45bff39d4ad323
nikhilagarwal1701 <nagarwal1701@gmail.com>
2022-08-04 23:08:03 +0530

    Starting from part 1 of Practical ML Tutorial with Python
    Stock Pricing regression model for Google stocks

---
+++ Interview Prep/Lean ML/Sentdex part 1/Stock pricing Model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import quandl
+import math
+
+df = quandl.get('WIKI/GOOGL')
+#df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = ((df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']) * 100
+df['PCT_change'] = ((df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']) * 100
+
+#df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace = True)
+
+forecast_out = int(math.ceil(0.001 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume', 'label']]
+df.dropna(inplace = True)
+print(df.head)
