1e51ca572c2bc23b45a0fc95a949e28264268662
nikhilagarwal1701 <nagarwal1701@gmail.com>
2022-09-08 01:05:22 +0530

    Liner regression and add comment in stock pricing model

--- Interview Prep/Lean ML/Sentdex/2. Linear Regression start.py
+++ Interview Prep/Lean ML/Sentdex/2. Linear Regression start.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 #df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -15,8 +17,24 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace = True)
 
 forecast_out = int(math.ceil(0.001 * len(df)))
+print(forecast_out)
+
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume', 'label']]
 df.dropna(inplace = True)
-print(df.head)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+# y = np.array(df['label'])  no idea why sentdex used this
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+# clf = svm.SVR() for using a diff classifier
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
