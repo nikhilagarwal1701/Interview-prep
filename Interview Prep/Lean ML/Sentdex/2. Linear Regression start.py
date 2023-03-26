763bb1baebe7a8868959a2221fcd808e39ed7381
nikhilagarwal1701 <nagarwal1701@gmail.com>
2023-03-26 17:45:23 +0530

    Liner regression and add comment in stock pricing model

--- Interview Prep/Lean ML/Sentdex/2. Linear Regression start.py
+++ Interview Prep/Lean ML/Sentdex/2. Linear Regression start.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 #df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -17,24 +21,47 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace = True)
 
 forecast_out = int(math.ceil(0.001 * len(df)))
+
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume', 'label']]
+
+X=np.array(df.drop(["label"],1))
+X=preprocessing.scale(X)
+X_lately=X[-forecast_out:]
+X = X[:-forecast_out:]
+
 df.dropna(inplace = True)
+y=np.array(df["label"])
+y=np.array(df["label"])
 
-X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 
-X = preprocessing.scale(X)
-# y = np.array(df['label'])  no idea why sentdex used this
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-clf = LinearRegression()
+clf = LinearRegression(n_jobs = -1)
 # clf = svm.SVR() for using a diff classifier
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. CLose'].plot()
+df['Forecast'].plot()
+plt.legend(loc = 4)
+plt.xlabel('Date')
+plt.ylabel('Price')
