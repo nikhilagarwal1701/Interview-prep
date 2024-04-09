b0419d33b4ff8d60e7db72dee6a63457e07ecbb0
nikhilagarwal1701 <nagarwal1701@gmail.com>
2024-04-10 00:53:52 +0530

    Runclub - Initial setup with database config

---
+++ SpringBoot/runclub/src/main/java/com/runclub/runclub/RunclubApplication.java
@@ -0,0 +1,13 @@
+package com.runclub.runclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunclubApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/runclub/src/test/java/com/runclub/runclub/RunclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runclub.runclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
